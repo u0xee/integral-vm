046c40368b4fabeabb155fbb70c513a1384577a9
Cole Frederick <csp.frederick@gmail.com>
2018-02-04 20:00:42 -0700

    Add initial java source in package.

---
+++ src/org/integral_vm/Codes.java
@@ -0,0 +1,8 @@
+// Static definition of symbolic names for the various bytecodes
+package org.integral_vm;
+
+public class Codes {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
